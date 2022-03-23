5f8a4644958fbdb98532b20962c837d66fc50f74
zgontea <228737@edu.p.lodz.pl>
2022-03-23 10:00:51 +0100

    added zgonte info

--- main.cpp
+++ main.cpp
@@ -2,6 +2,6 @@
 
 int main(){
     std::cout << "Team Name: ZakonKsiedzaDrRobaka\nLeader Role: Tester\nLeader Github ID: tomaszsztajkowski\n";
-
+    std::cout << "Developer #1: zgontea\n";
     return EXIT_SUCCESS;
 }
