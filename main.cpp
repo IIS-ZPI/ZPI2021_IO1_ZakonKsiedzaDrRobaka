abfa321036d3e1a7c057d8b6e267e207eab73d3d
FranciszekKarwowski <96189378+FranciszekKarwowski@users.noreply.github.com>
2022-03-23 09:56:31 +0100

    added printing my github ID

--- main.cpp
+++ main.cpp
@@ -2,6 +2,6 @@
 
 int main(){
     std::cout << "Team Name: ZakonKsiedzaDrRobaka\nLeader Role: Tester\nLeader Github ID: tomaszsztajkowski\n";
-
+    std::cout << "Developer #2: FranciszekKarwowski\n";
     return EXIT_SUCCESS;
 }
