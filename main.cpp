804b56c6e42ae7ba81cf39c92cfe86c707164292
Bartosz <228805@edu.p.lodz.pl>
2022-04-05 22:56:40 +0200

    added Addition

--- main.cpp
+++ main.cpp
@@ -1,8 +1,16 @@
 #include <iostream>
 
+double Addition(double A,double B);
+
 int main(){
-    std::cout << "Team Name: ZakonKsiedzaDrRobaka\nLeader Role: Tester\nLeader Github ID: tomaszsztajkowski\n";
+    std::cout << "Team Name: ZakonKsiedzaDrRobaka\nLeader Role: Tester\nLeader Github ID: tomaszsztajkowski\n";  
     std::cout << "DevOps: bartekmaka06\n";
+    std::cout << "Developer #1: zgontea\n";
     std::cout << "Developer #2: FranciszekKarwowski\n";
+    std::cout << "Tomasz Sztajkowski, Role: Tester, Github ID: tomaszsztajkowski\n";
+
     return EXIT_SUCCESS;
+}
+double Addition(double A,double B){
+    return A+B;
 }
