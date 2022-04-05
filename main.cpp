f1e87592b233e8c620277327e1b9ed145bd06c13
tomaszsztajkowski@gmail.com <tomaszsztajkowski@gmail.com>
2022-04-05 22:56:58 +0200

    division

--- main.cpp
+++ main.cpp
@@ -1,11 +1,18 @@
 #include <iostream>
 
+double division(double a, double b);
+
 int main() {
-    std::cout << "Team Name: ZakonKsiedzaDrRobaka\nLeader Role: Tester\nLeader Github ID: tomaszsztajkowski\n";  
+    std::cout << "Team Name: ZakonKsiedzaDrRobaka\nLeader Role: Tester\nLeader Github ID: tomaszsztajkowski\n";
     std::cout << "DevOps: bartekmaka06\n";
     std::cout << "Developer #1: zgontea\n";
     std::cout << "Developer #2: FranciszekKarwowski\n";
     std::cout << "Tomasz Sztajkowski, Role: Tester, Github ID: tomaszsztajkowski\n";
 
     return EXIT_SUCCESS;
+}
+
+double division(double a, double b) {
+    if (b == 0) return 0;
+    return a / b;
 }
