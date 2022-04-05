db5db706c9ce640f35a3c55c5ead4cc33d9897d8
Tomasz Sztajkowski <46276902+tomaszsztajkowski@users.noreply.github.com>
2022-04-05 22:35:40 +0200

    Revert "print name role and GitHub id (#3)"
    This reverts commit d32c5a90d54621262b0a1d7ce43d1ace3cfaeef1.

--- main.cpp
+++ main.cpp
@@ -2,7 +2,6 @@
 
 int main(){
     std::cout << "Team Name: ZakonKsiedzaDrRobaka\nLeader Role: Tester\nLeader Github ID: tomaszsztajkowski\n";
-    std::cout << "Tomasz Sztajkowski, Role: Tester, Github ID: tomaszsztajkowski\n";
 
     return EXIT_SUCCESS;
 }
