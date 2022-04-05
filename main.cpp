18459f20f36073c3bd3c8415bb9ab059f339e229
zgontea <228737@edu.p.lodz.pl>
2022-04-05 22:58:18 +0200

    added difference fun

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 
+double difference(double A, double B);
+
 int main() {
     std::cout << "Team Name: ZakonKsiedzaDrRobaka\nLeader Role: Tester\nLeader Github ID: tomaszsztajkowski\n";  
     std::cout << "DevOps: bartekmaka06\n";
@@ -7,4 +9,8 @@ int main() {
     std::cout << "Developer #2: FranciszekKarwowski\n";
   
     return EXIT_SUCCESS;
+}
+
+double difference(double A, double B) {
+    return A - B;
 }
