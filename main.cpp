d32c5a90d54621262b0a1d7ce43d1ace3cfaeef1
Tomasz Sztajkowski <46276902+tomaszsztajkowski@users.noreply.github.com>
2022-04-05 22:30:12 +0200

    print name role and GitHub id (#3)

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 
 int main(){
     std::cout << "Team Name: ZakonKsiedzaDrRobaka\nLeader Role: Tester\nLeader Github ID: tomaszsztajkowski\n";
+    std::cout << "Tomasz Sztajkowski, Role: Tester, Github ID: tomaszsztajkowski\n";
 
     return EXIT_SUCCESS;
 }
