fdc1abb15e69080e0073f6c4bfeaa0a53dee9eb5
Tomasz Sztajkowski <46276902+tomaszsztajkowski@users.noreply.github.com>
2022-04-05 23:14:16 +0200

    division (#10)
    * division
    
    * ctrl + alt + l
    
    * division
    
    * added division function

--- main.cpp
+++ main.cpp
@@ -1,16 +1,27 @@
 #include <iostream>
 
-double addition(double a,double b);
+
+double division(double a, double b);
+
+double addition(double a, double b);
+
 
 int main() {
-    std::cout << "Team Name: ZakonKsiedzaDrRobaka\nLeader Role: Tester\nLeader Github ID: tomaszsztajkowski\n";  
+    std::cout << "Team Name: ZakonKsiedzaDrRobaka\nLeader Role: Tester\nLeader Github ID: tomaszsztajkowski\n";
     std::cout << "DevOps: bartekmaka06\n";
     std::cout << "Developer #1: zgontea\n";
     std::cout << "Developer #2: FranciszekKarwowski\n";
-    std::cout << "Tomasz Sztajkowski, Role: Tester, Github ID: tomaszsztajkowski\n";
+    std::cout << "Tester #3: tomaszsztajkowski\n";
 
     return EXIT_SUCCESS;
 }
-double addition(double a,double b){
-    return a+b;
+
+
+double division(double a, double b) {
+    if (b == 0) return 0;
+    return a / b;
+}
+
+double addition(double a, double b) {
+    return a + b;
 }
